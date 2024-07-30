4dee1a923ec0a145ef727755f6cf083dccf80415
AnushaDonthula <162703733+AnushaDonthula@users.noreply.github.com>
2024-07-30 22:54:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(ropes) {
+    if (ropes.length === 0) return 0;
+
+    // Min-Heap implementation
+    function MinHeap() {
+        this.heap = [];
+    }
+    MinHeap.prototype.insert = function(val) {
+        this.heap.push(val);
+        this.heap.sort((a, b) => a - b);
+    };
+    MinHeap.prototype.remove = function() {
+        return this.heap.shift();
+    };
+    MinHeap.prototype.size = function() {
+        return this.heap.length;
+    };
+
+    const minHeap = new MinHeap();
+    for (let length of ropes) {
+        minHeap.insert(length);
+    }
+
+    let totalCost = 0;
+
+    while (minHeap.size() > 1) {
+        let first = minHeap.remove();
+        let second = minHeap.remove();
+        let cost = first + second;
+        totalCost += cost;
+        minHeap.insert(cost);
+    }
+
+    return totalCost;
 }
 
-module.exports=mincost;
+// Function to handle button click and display the result
+function calculateMinCost() {
+    const input = document.getElementById('ropeLengths').value;
+    const ropeLengths = input.split(',').map(Number);
+    const result = mincost(ropeLengths);
+    document.getElementById('result').textContent = `Minimum Cost: ${result}`;
+}
